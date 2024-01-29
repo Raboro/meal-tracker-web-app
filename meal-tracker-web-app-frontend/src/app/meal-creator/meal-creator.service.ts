49308399b85d41be08b2bc6844b7c86fd01db6ef
Raboro <marius.woerfelreal@gmail.com>
2024-01-29 21:07:49 +0100

    created MealCreatorService

---
+++ meal-tracker-web-app-frontend/src/app/meal-creator/meal-creator.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Meal } from '../../types/Meal';
+import { environment } from '../../environment/environment';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MealCreatorService {
+  private constructor(private httpClient: HttpClient) {}
+
+  addMeal(meal: Meal): Observable<Meal> {
+    return this.httpClient.post<Meal>(`${environment.API_BAE_DIR}/meal`, meal);
+  }
+}
