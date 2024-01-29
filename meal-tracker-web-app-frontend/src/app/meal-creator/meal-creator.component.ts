4c6a1c0aa894c133b3ce7bdb04ff230bf67584b1
Raboro <marius.woerfelreal@gmail.com>
2024-01-29 21:08:08 +0100

    included http in MealCreator and enhanced form with fields

--- meal-tracker-web-app-frontend/src/app/meal-creator/meal-creator.component.ts
+++ meal-tracker-web-app-frontend/src/app/meal-creator/meal-creator.component.ts
@@ -1,12 +1,19 @@
+import { MealCreatorService } from './meal-creator.service';
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-meal-creator',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './meal-creator.component.html',
-  styleUrl: './meal-creator.component.css'
+  styleUrl: './meal-creator.component.css',
 })
 export class MealCreatorComponent {
+  name = new FormControl('');
+  date = new FormControl(new Date());
 
+  constructor(service: MealCreatorService) {}
+
+  addMeal(): void {}
 }
